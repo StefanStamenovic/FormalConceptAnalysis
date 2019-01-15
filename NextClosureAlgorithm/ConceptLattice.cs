213bd350b92e27e93a1ea8a768935e7ee6961775
Nenad Stojanovic <nenad.stojanovic@gmx.com>
2019-01-15 02:50:33 +0100

    asd

--- NextClosureAlgorithm/ConceptLattice.cs
+++ NextClosureAlgorithm/ConceptLattice.cs
@@ -8,7 +8,7 @@ using System.IO;
 
 namespace NextClosureAlgorithm
 {
-    public class ConceptLattice
+    public class ConceptLattice 
     {
         private FormalContext context { get; set; }
         /// <summary>
